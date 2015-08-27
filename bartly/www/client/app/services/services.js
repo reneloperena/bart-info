8386182236f1013137e0812bd89d3d6bc2dd4be3
Rene Loperena <rene@implugd.com>
2015-08-27 13:40:49 -0700

    Modifies services to use BART API directly

--- bartly/www/client/app/services/services.js
+++ bartly/www/client/app/services/services.js
@@ -2,11 +2,11 @@ var services = angular.module( 'bartServices', []);
 
 services.factory('Stations', function($http) {
   var getAll = function() {
-    return $http({method:'GET', url:"http://localhost:8080/api/stations"});
+    return $http({method:'GET', url:'http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V'});
   };
 
   var getSchedules = function(station) {
-    return $http({method:'GET', url:"http://localhost:8080/api/stations/"+station});
+    return $http({method:'GET', url:'http://api.bart.gov/api/etd.aspx?cmd=etd&orig='+station+'&key=MW9S-E7SL-26DU-VV8V'});
   };
 
   return { getAll:getAll, getSchedules:getSchedules};
