80b9ada4b194d352b61e7ce9212b445d7dbd2a92
Rene Loperena <rene@implugd.com>
2015-08-29 17:09:26 -0700

    Adds refresh to stationList

--- bartly/www/client/app/main/stationList.js
+++ bartly/www/client/app/main/stationList.js
@@ -3,23 +3,25 @@ var list = angular.module('stations', ['bartServices']);
 list.controller('listController', function(Stations, States, $scope, $location) {
   var x2js = new X2JS();
   $scope.stations = [];
- 
-  Stations.getAll().then(function(res) {
-    var result = x2js.xml_str2json(res.data);
-    var stations = [];
+  $scope.loadStationList = function(){
+    Stations.getAll().then(function(res) {
+      var result = x2js.xml_str2json(res.data);
+      var stations = [];
 
-    xmlStations = result.root.stations.station;
+      xmlStations = result.root.stations.station;
 
-    for (var i = 0; i < xmlStations.length; i++) {
-      stations.push({
-        name: xmlStations[i].name,
-        id: xmlStations[i].abbr
-      });
-    };
-
-    //For each one of the stations will take the name and the abbr to create a new station object
-    $scope.stations = stations;
-  });
+      for (var i = 0; i < xmlStations.length; i++) {
+        stations.push({
+          name: xmlStations[i].name,
+          id: xmlStations[i].abbr
+        });
+      };
+      $scope.stations = stations;
+      $scope.$broadcast('scroll.refreshComplete');
+      
+    });
+  };
+  $scope.loadStationList();
 
   $scope.setCurrentStation = function(station){
     States.setCurrentStation(station);
